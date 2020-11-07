b0f359af7206d7145b280d928f840573f99a72b7
Absurdponcho <60100776+Absurdponcho@users.noreply.github.com>
2020-11-07 13:58:21 +1100

    Update main.c

--- bootloader/main.c
+++ bootloader/main.c
@@ -2,8 +2,21 @@
 #include <efilib.h>
 
 EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-    InitializeLib(ImageHandle, SystemTable);
-    Print(L"Hello, world!\n");
+	EFI_STATUS Status;
+	EFI_INPUT_KEY Key;
 
-    return EFI_SUCCESS;
+	ST = SystemTable; //Store the system table
+
+	Status = ST->ConOut->OutputString(ST->ConOut, L"HelloWorld!\n\r"); // Print HelloWorld to the console
+
+	if (EFI_ERROR(Status)) //If printing failed, return
+		return Status;
+
+	Status = ST->ConIn->Reset(ST->ConIn, FALSE); // Empty the Console Input Buffer
+	if (EFI_ERROR(Status))
+		return Status;
+
+	while ((Status = ST->ConIn->ReadKeyStroke(ST->ConIn, &Key)) == EFI_NOT_READY) ; // Poll For Key Input
+
+	return Status; // Exit the UEFI application
 }
